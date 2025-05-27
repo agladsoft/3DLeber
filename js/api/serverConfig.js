741b260d3d0e992aea1c3421f225007b1a39d5fd
timur <agladsoft@gmail.com>
2025-05-27 16:43:25 +0300

    fix: добавлен вывод API_BASE_URL в serverConfig.js для отладки конфигурации сервера

--- js/api/serverConfig.js
+++ js/api/serverConfig.js
@@ -2,6 +2,7 @@
 const SERVER_IP = process.env.SERVER_IP || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || '3000';
 const API_BASE_URL = `http://${SERVER_IP}:${SERVER_PORT}/api`;
+console.log(API_BASE_URL)
 const POSTGRES_HOST = process.env.POSTGRES_HOST || 'postgres';
 const POSTGRES_DB = process.env.POSTGRES_DB || 'admin';
 const POSTGRES_USER = process.env.POSTGRES_USER || 'admin';
